bc05e37ac5f1da608b76fe77eb4707d0b64c979c
toptaab <top_giveheart@hotmail.com>
2025-08-14 15:09:44 +0700

    add api code and dynamic status code interceptor success

--- src/common/interceptors/success.interceptor.ts
+++ src/common/interceptors/success.interceptor.ts
@@ -8,11 +8,17 @@ import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class SuccessInterceptor implements NestInterceptor {
-  intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const ctx = context.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
     return next.handle().pipe(
       map((data) => ({
-        statusCode: 200,
-        message: 'Success',
+        statusCode: response.statusCode,
+        apiCode: request.apiCode || 'SUCCESS',
+        message: request.apiMessage || 'Success',
+        timestamp: new Date().toISOString(),
         data,
       })),
     );
