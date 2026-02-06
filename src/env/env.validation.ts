be64c664e29ba3236183963708b9511d712b47d2
toptaab <120720374+Toptaab@users.noreply.github.com>
2026-02-06 11:37:33 +0700

    add env example and validator

---
+++ src/env/env.validation.ts
@@ -0,0 +1,73 @@
+import { plainToInstance } from 'class-transformer';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  validateSync,
+} from 'class-validator';
+
+export enum Environment {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+  Provision = 'provision',
+}
+
+class EnvironmentVariablesDto {
+  @IsNotEmpty()
+  @IsNumber()
+  PORT: number;
+
+  @IsOptional()
+  @IsEnum(Environment)
+  NODE_ENV: Environment;
+
+  @IsNotEmpty()
+  @IsString()
+  JWT_SECRET: string;
+
+  @IsOptional()
+  @IsNumber()
+  HASH_SALT: number;
+
+  @IsNotEmpty()
+  @IsString()
+  DATABASE_URL: string;
+
+  @IsNotEmpty()
+  @IsString()
+  CORS_ORIGIN: string;
+
+
+  @IsOptional()
+  @IsString()
+  GOOGLE_CLIENT_ID: string;
+
+  @IsOptional()
+  @IsString()
+  GOOGLE_CLIENT_SECRET: string;
+
+  @IsOptional()
+  @IsString()
+  FACEBOOK_CLIENT_ID: string;
+
+  @IsOptional()
+  @IsString()
+  FACEBOOK_CLIENT_SECRET: string;
+
+}
+
+export const validate = (config: Record<string, unknown>) => {
+  const validatedConfig = plainToInstance(EnvironmentVariablesDto, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+};
